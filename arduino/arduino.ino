390369f26be183762fcefd73beda24af26b88e2e
isacleibe <39234921+isacleibe@users.noreply.github.com>
2018-11-13 19:52:23 +0100

    Arduino y Python, falta eclipse

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,19 +1,31 @@
 // --------------------------------------------------------------------------------------
 //                                DOMOTIC ROOM
 // --------------------------------------------------------------------------------------
-#include <IRremote.h>
-IRsend irsend;
+
+//#include <IRremote.h>
+//IRsend irsend;
 // Estados del sistema
-const int NOCONTROL = 0;
-const int LUZCONTROL = 1;
-const int TEMPCONTROL = 2;
-const int ALLCONTROL = 3;
-const int ONLUZ = 4;
-const int OFFLUZ = 5;
-const int ONTEMP = 6;
-const int OFFTEMP = 7;
+const char NOCONTROL = 'a';
+const char LUZCONTROL = 'b';
+const char ONLUZ = 'c';
+const char OFFLUZ = 'd';
+
+// PINES
+const byte tempPin = A0;
+const byte luzPin = A1;
+const byte releluzPin = 2;
+
+// PERIODOS
+const unsigned long anPer = 2000;
+const unsigned long monitorPer = 5000;
+const unsigned long sistemaPer = 5000;
+const unsigned long relePer = 5000;
+
+//UMBRALES
+const float UMBRALLUZ = 400;
+
 // --------------------------------------------------------------------------------------
-//                                Entrada Analogica Periodica
+//                            ENTRADA ANALOGICA PERIODICA
 // --------------------------------------------------------------------------------------
 struct EntradaAnalogica{
   byte pin;
@@ -29,50 +41,51 @@ void setup_EntradaAnalogica (struct EntradaAnalogica& e, byte pin, unsigned long
   e.estado = analogRead(e.pin);
 }
 
+
 void loop_EntradaAnalogica (struct EntradaAnalogica& e, unsigned long currms) {
   if (currms - e.lastms >= e.perms) {
     e.lastms += e.perms;
     e.estado = analogRead(e.pin);
   }
 }
+
 // --------------------------------------------------------------------------------------
-//                                      Monitor
+//                                    MONITOR
 // --------------------------------------------------------------------------------------
 struct Monitor{
   unsigned long perms;
   unsigned long lastms;
 
-  char mensaje;
-  float umbralTemp;
+  char mensaje;  
   float umbralLuz;
 };
 void setup_Monitor (struct Monitor& m, unsigned long perms, unsigned long currms) {
   Serial.begin(9600);
   m.perms = perms;
   m.lastms = currms - m.perms;  
-  m.umbralLuz = UMBRALLUZ;
-  m.umbralTemp = UMBRALTEMP;
+  m.umbralLuz = UMBRALLUZ;  
 }
 
-void loop_Monitor(struct Monitor& m, const struct EntradaAnalogica& eluz, const struct EntradaAnalogica& etemp, unsigned long currms) {
+
+void loop_Monitor(struct Monitor& m, const struct EntradaAnalogica& eluz, unsigned long currms) {
   if(currms - m.lastms >= m.perms) {
     //blink(3);
     m.lastms += m.perms;
     
     // RECIBE DATOS
     if (Serial.available()>0) {
-      blink(4);
+      blink(1);
       m.mensaje = Serial.read();      
     }
         
     // ENVIA DATOS: temperatura y luminosidad.     
-    Serial.println(eluz.estado);
-    Serial.println(etemp.estado);
+    Serial.println(eluz.estado);   
   }
   
 }
+
 // --------------------------------------------------------------------------------------
-//                                      Sistema
+//                                    SISTEMA
 // --------------------------------------------------------------------------------------
 struct Sistema {
   int estado;
@@ -81,20 +94,18 @@ struct Sistema {
   unsigned long perms;
   unsigned long lastms;
 
-  boolean luzencender;
-  boolean tempencender;
+  boolean luzencender;  
 };
 void setup_Sistema(struct Sistema& s, unsigned long perms, unsigned long currms){
   s.estado = NOCONTROL;
   s.perms = perms;
   s.lastms = currms - s.perms;
-  s.luzencender = false;
-  s.tempencender = false;
+  s.luzencender = false;  
 }
 
+
 void loop_Sistema(struct Sistema& s, 
-                  const struct EntradaAnalogica& eluz, 
-                  const struct EntradaAnalogica& etemp, 
+                  const struct EntradaAnalogica& eluz,                   
                   const struct Monitor& m, 
                   unsigned long currms){
                     
@@ -106,29 +117,10 @@ void loop_Sistema(struct Sistema& s,
       s.luzencender = true;
     } else {
       s.luzencender = false;
-    }
-    if(etemp.estado < m.umbralTemp){
-      s.tempencender = true;
-    } else {
-      s.tempencender = false;
-    }
+    }    
 
-    // El estado solo va a cambiar dependiendo de un caracter que envia el monitor.
-    switch (m.mensaje) {
-      case 'a':
-        s.estado = NOCONTROL;
-      break;
-      case 'b':
-        s.estado = LUZCONTROL;
-      break;
-      case 'c':
-        s.estado = TEMPCONTROL;
-      break;
-      case 'd':
-        s.estado = ALLCONTROL;
-      break;
-    }
-    //s.estado = m.mensaje; Terminariamos más rapido así si s.estado fuese un char.
+    // El estado solo va a cambiar dependiendo de un caracter que envia el monitor.    
+    s.estado = m.mensaje;
     
     if(s.estadoant != s.estado) {
       s.cambioms = currms;
@@ -137,11 +129,69 @@ void loop_Sistema(struct Sistema& s,
 }
 
 // --------------------------------------------------------------------------------------
+//                                 RELE LUZ ESTUDIO
+// --------------------------------------------------------------------------------------
+struct ReleLuz{
+  byte pin;
+  byte estado;
+  unsigned long perms;
+  unsigned long lastms;
+  unsigned long cambioms;
+};
+void setup_ReleLuz(struct ReleLuz& r, byte pin, unsigned long perms, unsigned long currms) {
+  r.pin = pin;
+  r.perms = perms;
+  r.lastms = currms - r.perms;
+  
+  pinMode(r.pin, OUTPUT);
+  r.estado = 0;
+  digitalWrite(r.pin, r.estado);
+}
+
+
+void loop_ReleLuz(struct ReleLuz& r, const struct Sistema& sis, unsigned long currms) {
+  if(currms - r.lastms >= r.perms) {
+    r.lastms += r.perms;
+    // AJUSTE POR ESTADOS
+    if((sis.estado == LUZCONTROL) && sis.luzencender == true) {
+      r.estado = HIGH;
+      r.cambioms = currms;
+    } else {
+      r.estado = LOW;
+      r.cambioms = currms;
+    }    
+    if(sis.estado == ONLUZ) {
+      r.estado = HIGH;
+      r.cambioms = currms;
+    } else if(sis.estado == OFFLUZ) {
+      r.estado = LOW;
+      r.cambioms = currms;
+    }
+  }
+  if(r.cambioms == currms){
+    digitalWrite(r.pin, r.estado);
+  }
+}
+
+// --------------------------------------------------------------------------------------
+//                                      DECLARACION
+// --------------------------------------------------------------------------------------
+struct EntradaAnalogica sensorL;
+struct Monitor monitor;
+struct Sistema sistema;
+struct ReleLuz releluz;
+
+// --------------------------------------------------------------------------------------
 //                                      SETUP
 // --------------------------------------------------------------------------------------
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(13, OUTPUT);
+  unsigned long currms = millis();  
+  setup_EntradaAnalogica(sensorL, luzPin, anPer, currms);  
+  setup_Monitor(monitor, monitorPer, currms);
+  setup_Sistema(sistema, sistemaPer, currms);
+  setup_ReleLuz(releluz, releluzPin, relePer, currms);  
 }
 
 
@@ -150,5 +200,25 @@ void setup() {
 // --------------------------------------------------------------------------------------
 void loop() {
   // put your main code here, to run repeatedly:
+  unsigned long currms = millis();  
+  loop_EntradaAnalogica(sensorL, currms);  
+  loop_Monitor(monitor, sensorL, currms);
+  loop_Sistema(sistema, sensorL, monitor, currms);
+  loop_ReleLuz(releluz, sistema, currms);  
+}
 
+
+
+// funciones para depurar usando leds
+void blink(unsigned int numberTimes) {
+  blink(numberTimes, 100);
+}
+
+void blink(unsigned int numberTimes, int delayT) {
+  for ( int i = 0; i < numberTimes; i++) {
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(delayT);              // wait for a second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(delayT);
+  }
 }
---
+++ lanza.cmd
@@ -0,0 +1,1 @@
+python domoticRoomPython.py --registry_id=domotic-registro --cloud_region=europe-west1 --project_id=domotic-room  --device_id=domotic-dispositivo --algorithm=RS256 --private_key_file=rsa_private.pem
